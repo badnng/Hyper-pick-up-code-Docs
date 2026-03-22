f43c0e839e041c525ac8c6ed62eba10f69757ffc
Badnng <wsj316@outlook.com>
2026-03-22 14:31:58 +0800

    初始化 VitePress 文档站

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: 'Hyper-pick-up-code',
+  title: 'Docs',
+  description: '文档站点',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/getting-started' }
+    ],
+    sidebar: [
+      {
+        text: '指南',
+        items: [
+          { text: '快速开始', link: '/guide/getting-started' }
+        ]
+      }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+layout: home
+hero:
+  name: 我的文档站
+  text: 欢迎
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/getting-started
+---
